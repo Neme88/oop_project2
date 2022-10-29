183878e549b0024790b8a05a7437f852c6f93f5c
Neme Nwaka <ccnwaka1988@gmail.com>
2022-10-29 12:24:22 +0200

    created Teller class

---
+++ oop_project2.py
@@ -0,0 +1,17 @@
+class Teller (Person):
+    def __init__(self, id, name, address, phone_number):
+        super().__init__(id, name, address, phone_number)
+    def collectMoney():
+        pass
+    def openAccount():
+        pass
+    def closeAccount():
+        pass
+    def loanRequest():
+        pass
+    def provideInfo():
+        pass
+    def issueCard():
+        pass
+    
+        pass
