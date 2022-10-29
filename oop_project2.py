68f2e4125accfbad6c71be81dfaf53428063a92f
Neme Nwaka <ccnwaka1988@gmail.com>
2022-10-29 13:16:28 +0200

    created Loan class.

--- oop_project2.py
+++ oop_project2.py
@@ -24,4 +24,10 @@ class Checking (Account):
 class Savings (Account):
     def __init__(self, id, customerId):
         super().__init__(id, customerId)
+class Loan :
+    def __init__(self, id, type, accountId, customerId) :
+        self.id = id
+        self.type = type
+        self.accountId = accountId
+        self.customerId = customerId
         
